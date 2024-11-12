5176be2e22b495d4689677e42ec7984e8f7337fe
Ayushman Mishra <132093864+Ayushman88@users.noreply.github.com>
2024-11-12 19:04:12 +0530

    feat: add routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,21 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Highlights from "./pages/Highlights";
+import Schedule from "./pages/Schedule";
+import Registration from "./pages/Registration";
+
 function App() {
   return (
-    <>
-      <div className="bg-red-700 text-white">Hello</div>
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/highlights" element={<Highlights />} />
+        <Route path="/schedule" element={<Schedule />} />
+        <Route path="/registration" element={<Registration />} />
+      </Routes>
+    </Router>
   );
 }
 
