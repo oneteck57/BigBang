318d5b370dedd47095616b14f4e113167da4f619
Ayushman Mishra <132093864+Ayushman88@users.noreply.github.com>
2024-11-13 19:47:16 +0530

    feat: add about page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,11 @@
+import About from "../components/Home/About";
 import Landing from "../components/Home/Landing";
 
 const Home = () => {
   return (
     <>
       <Landing />
+      <About />
     </>
   );
 };
