545e2dd6a40e1097c321c5c71603fe1b7b0c90c8
Ayushman Mishra <132093864+Ayushman88@users.noreply.github.com>
2024-11-14 17:31:14 +0530

    feat: add gsap animation

--- src/components/Home/Landing.jsx
+++ src/components/Home/Landing.jsx
@@ -1,6 +1,8 @@
 import { useEffect, useRef } from "react";
+import "../../App.css";
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
+import Header from "../Global/Header";
 
 gsap.registerPlugin(ScrollTrigger);
 
@@ -17,9 +19,11 @@ const Landing = () => {
   const globeRef = useRef(null);
 
   useEffect(() => {
-    const roadElement = roadRef.current;
     const busElement = busRef.current;
     const globeElement = globeRef.current;
+    const roadElement = roadRef.current;
+    const boyElement = document.querySelector("img[alt='boy']");
+    const brainElement = document.querySelector("img[alt='brainyoga']");
 
     gsap.to(busElement, {
       x: "-100vw",
@@ -43,10 +47,27 @@ const Landing = () => {
         scrub: true,
       },
     });
+
+    gsap.to(boyElement, {
+      x: "50vw",
+      repeat: -1,
+      yoyo: true,
+      duration: 2,
+      ease: "power3.inOut",
+    });
+
+    gsap.to(brainElement, {
+      y: "2vw",
+      repeat: -1,
+      yoyo: true,
+      duration: 2,
+      ease: "power3.inOut",
+    });
   }, []);
 
   return (
     <>
+      <Header />
       <div className="landing h-[140vh] bg-[#2F2F2F] relative overflow-hidden flex flex-col items-center justify-center text-center text-white">
         {/* Globe */}
         <img
@@ -124,8 +145,8 @@ const Landing = () => {
         {/* Brain Yoga */}
         <img
           src="/assets/brainYoga.png"
-          alt="brain yoga"
-          className="absolute bottom-[25rem] left-40 select-none"
+          alt="brainyoga"
+          className="absolute bottom-[26rem] left-40 select-none"
           draggable="false"
           style={{
             width: "140px",
@@ -135,12 +156,14 @@ const Landing = () => {
         />
 
         {/* Title, Button, and Paragraph */}
-        <div className="relative z-20 mt-24 flex flex-col items-center">
-          <h1 className="text-8xl font-bold mb-4">THE BIG BANG</h1>
-          <button className="bg-[#F3EDD3] border-2 border-black text-black px-6 py-2 rounded-full mb-4 font-semibold hover:bg-yellow-600 transition-colors">
+        <div className="relative z-20 mt-24 flex flex-col items-center luckiest-guy-small">
+          <h1 className="text-8xl font-bold mb-4 luckiest-guy-regular luckiest-guy-outline">
+            THE BIG BANG
+          </h1>
+          <button className="bg-[#F3EDD3] font-sans text-lg font-normal border-2 border-black text-black px-6 py-2 rounded-full mb-4  hover:bg-[#fcf0bd]  transition-colors">
             Get your Tickets
           </button>
-          <p className="text-lg font-medium italic mt-10">
+          <p className="text-lg text-[#F3EDD3] font-medium italic mt-10 tracking-widest ">
             {`"Two causes, one purpose: Empowering people, healing planet"`}
           </p>
         </div>
@@ -176,7 +199,6 @@ const Landing = () => {
           />
         ))}
       </div>
-
       {/* Road Image Container */}
       <div ref={roadRef} className="w-full bg-[#2F2F2F] relative">
         {/* Bus Image */}
