a48a17df8a5654ef9b71c6293c4851436caa5d09
Ayushman Mishra <132093864+Ayushman88@users.noreply.github.com>
2024-11-23 22:10:41 +0530

    feat: add about page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,5 +1,21 @@
+import AboutInfo from "../components/About/AboutInfo";
+import Header from "../components/Global/Header";
+import EarthCard from "../components/About/EarthCard";
+import Perks from "../components/About/Perks";
+import Faq from "../components/About/Faq";
+
 const About = () => {
-  return <div>About</div>;
+  return (
+    <>
+      <Header />
+      <div className="relative">
+        <AboutInfo />
+        <EarthCard />
+        <Perks />
+        <Faq />
+      </div>
+    </>
+  );
 };
 
 export default About;
