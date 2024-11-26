91cf1dc9307a7c5973e2b97aa1f9a4433f0dc4f9
Ayushman Mishra <132093864+Ayushman88@users.noreply.github.com>
2024-11-26 19:27:01 +0530

    feat: add bg/text color props

--- src/components/Global/Header.jsx
+++ src/components/Global/Header.jsx
@@ -1,10 +1,16 @@
+/* eslint-disable react/prop-types */
 import { NavLink } from "react-router-dom";
 import "../../App.css";
-const Header = () => {
+
+const Header = ({ bgColor = "bg-transparent", textColor = "text-white" }) => {
   return (
-    <header className="absolute top-10 left-1/2 transform -translate-x-1/2 bg-transparent z-50 py-4 px-10">
+    <header
+      className={`absolute w-full flex justify-center items-center top-0 pt-20 left-1/2 transform -translate-x-1/2 ${bgColor} z-10 py-4 px-10`}
+    >
       <nav className="flex justify-between items-center">
-        <ul className="flex space-x-20 text-white text-xl  luckiest-guy-small-outline">
+        <ul
+          className={`flex space-x-20 ${textColor} text-xl luckiest-guy-small-outline`}
+        >
           <li>
             <NavLink to="/">Home</NavLink>
           </li>
